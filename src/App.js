a52da6a063927802c21368e4c639744f1bceff68
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-08-10 16:50:53 -0400

    troubleshooting

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(window.location.href);
   return (
     <div className="App">
       <header className="App-header">
