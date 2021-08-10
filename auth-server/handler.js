a9704c344e4f61c35a6bf57ce4a4f13d766565c5
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-08-10 14:56:23 -0400

    attempting to fix cors error: -removed header from getAccessToken in handlers.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -58,11 +58,9 @@ module.exports.getAccessToken = async (event) => {
     });
   })
     .then((token) => {
+      // Respond with OAuth token
       return {
         statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
         body: JSON.stringify(token),
       };
     })
