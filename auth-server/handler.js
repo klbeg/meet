e26d32aecf61441e8dd4d3482ca59292ef385b53
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-08-10 14:57:58 -0400

    added header again...

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -58,9 +58,11 @@ module.exports.getAccessToken = async (event) => {
     });
   })
     .then((token) => {
-      // Respond with OAuth token
       return {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
         body: JSON.stringify(token),
       };
     })
