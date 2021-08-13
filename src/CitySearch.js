563d3cc468ee72ecdd8054753277323f951b6bdf
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-08-12 20:44:54 -0400

    City Search test passed along with App.test and EventList.test

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 
 class CitySearch extends Component {
+  state = {
+    query: '',
+    suggestions: [],
+  };
+
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    this.setState({
+      query: value,
+      suggestions: [],
+    });
+  };
+
   render() {
-    return <div className="CitySearch"></div>;
+    return (
+      <div className="CitySearch">
+        <input
+          className="city"
+          value={this.state.query}
+          onChange={this.handleInputChange}
+          type="text"
+        />
+        <ul className="suggestions">
+          {this.state.suggestions.map((suggestion) => (
+            <li key={suggestion}>{suggestion}</li>
+          ))}
+          <li key="all">
+            <b>See all cities</b>
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
 
