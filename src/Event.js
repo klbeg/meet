1274afa5e5c23011dfda707c6401efe38f085f71
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-08-16 17:28:20 -0400

    added integrated testing to App.testing -events sucessfully passed from App.js to EventList.js √- locations passed from App.js to CitySearch.js

--- src/Event.js
+++ src/Event.js
@@ -5,43 +5,26 @@ class Event extends Component {
   state = {
     showDetails: undefined,
   };
-  handleShowDetails = () => {
+  handleShowDetails = (value) => {
     this.setState({
-      showDetails: true,
+      showDetails: value,
     });
   };
-
-  //  added this back.  If I call this.handleShowDetails(variable),
-  //  the function doesn't actually work right.  it won't set the state properly.
-  //  I'm guessing I need to use a different syntax in the onClick handler
-  //  for things to work correctly.  Going to look into it
-  handleHideDetails = () => {
-    this.setState({
-      showDetails: false,
-    });
-  };
-
-  /*
-    Here's the function I would like to have controll show/hide details
-
-    toggleShowDetails = () => {
-      this.setState({
-        showDetails: !this.showDetails 
-        //  essentially, set showDetails to whatever it is not
-        //  if it's truthy, make it falsy and visa versa
-      })
-    }
-
-  */
 
   render() {
     return (
       <div className="event">
         {this.state.showDetails ? <EventDetails /> : <div />}
-        <button className="show-details" onClick={this.handleShowDetails}>
+        <button
+          className="show-details"
+          onClick={() => this.handleShowDetails(true)}
+        >
           Show Details
         </button>
-        <button className="hide-details" onClick={this.handleHideDetails}>
+        <button
+          className="hide-details"
+          onClick={() => this.handleShowDetails(false)}
+        >
           Hide Details
         </button>
       </div>
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import { mockData } from '../mock-data';
+import { extractLocations } from '../api';
 
 describe('<App /> component', () => {
   let AppWrapper;
@@ -16,5 +18,26 @@ describe('<App /> component', () => {
 
   test('render CitySearch', () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+});
+
+describe('<App /> integration', () => {
+  test('App passes "events" state as a prop to EventList', () => {
+    const AppWrapper = mount(<App />);
+    const AppEventsState = AppWrapper.state('events');
+
+    expect(AppEventsState).not.toEqual(undefined);
+    expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
+    AppWrapper.unmount();
+  });
+  test('App passes "locations" state as a prop to CitySearch', () => {
+    const AppWrapper = mount(<App />);
+    const AppLocationsState = AppWrapper.state('locations');
+
+    expect(AppLocationsState).not.toEqual(undefined);
+    expect(AppWrapper.find(CitySearch).props().locations).toEqual(
+      AppLocationsState
+    );
+    AppWrapper.unmount();
   });
 });
