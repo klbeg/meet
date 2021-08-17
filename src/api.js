bcf9313785c730c845e308a7d20f9c03e9219cdc
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-08-17 15:54:47 -0400

    commiting to start troubleshooting missing config.json

--- src/api.js
+++ src/api.js
@@ -1,4 +1,20 @@
+import axios from 'axios';
 import { mockData } from './mock-data';
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem('access_token');
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get('code');
+    if (!code) {
+      const results = await axios.get('');
+    }
+  }
+};
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
