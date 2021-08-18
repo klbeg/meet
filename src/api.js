fefc355a9001fd94e8d806991be94bc6b272a1a9
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-08-18 12:40:49 -0400

    troubleshooting: -removed return statement from getToken fetch response

--- src/api.js
+++ src/api.js
@@ -14,7 +14,7 @@ const getToken = async (code) => {
   const { access_token } = await fetch(getTokenUrl + '/' + encodeCode)
     .then((res) => {
       console.log('getToken response: ', res.json());
-      return res.json();
+      res.json();
     })
     .then((access_token_test) => {
       console.log(access_token_test);
