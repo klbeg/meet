a71564cca3d0b326d72b054264d18ab952be3e1b
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-08-18 15:33:13 -0400

    troubleshooting

--- src/api.js
+++ src/api.js
@@ -19,7 +19,7 @@ const getToken = async (code) => {
     })
     .catch((error) => error);
 
-  console.log('post promise value for "access_token": ', access_token);
+  console.log('post promise poop for "access_token": ', access_token);
   access_token && localStorage.setItem('access_token', access_token);
 
   return access_token;
