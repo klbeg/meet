83faf382b1c4febe497a7d77f51c50a793eb0615
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-08-18 11:36:01 -0400

    api.js: -returned code const with original code index.html: -removed reverence to logo192.png

--- src/api.js
+++ src/api.js
@@ -30,9 +30,7 @@ export const getAccessToken = async () => {
   if (!accessToken || tokenCheck.error) {
     await localStorage.removeItem('access_token');
     const searchParams = new URLSearchParams(window.location.search);
-    //const code = await searchParams.get('code');
-    const code =
-      '4%2F0AX4XfWi6uLxan5QeYbfd3P0MNFIBTnK5w_hKR_JldSuz8cVbr8ocrzlfp59aedJeRrNFlg';
+    const code = await searchParams.get('code');
     if (!code) {
       const results = await axios.get(
         'https://309jzcntd7.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url'
