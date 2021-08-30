a0e1688af333bf6dfd8f8940480228f00789ee5e
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-08-30 13:01:45 -0400

    fixed Event.test

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -4,27 +4,35 @@ import { shallow } from 'enzyme';
 import Event from '../Event';
 import EventDetails from '../EventDetails';
 
-//  talk with john about how to fix on Friday
+describe('<EventDetails /> component', () => {
+  let event = {
+    summary: 'Learn JavaScript',
+    description:
+      'Have you wondered how you can ask Google to show you the list of the top ten must-see places in London? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these. :) \n\nJavascript offers interactivity to a dull, static website. Come, learn JavaScript with us and make those beautiful websites.',
+    location: 'London, UK',
+    start: {
+      dateTime: '2020-05-19T16:00:00+02:00',
+      timeZone: 'Europe/Berlin',
+    },
+  };
+  let EventWrapper;
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={event} />);
+  });
 
-// describe('<EventDetails /> component', () => {
-//   let EventWrapper;
-//   beforeAll(() => {
-//     EventWrapper = shallow(<Event />);
-//   });
+  test('Event details should be collapsed by default', () => {
+    expect(EventWrapper.find(EventDetails)).toHaveLength(0);
+  });
 
-//   test('Event details should be collapsed by default', () => {
-//     expect(EventWrapper.find(EventDetails)).toHaveLength(0);
-//   });
+  test('can click show details to expand EventDetails', () => {
+    EventWrapper.setState({ showDetails: false });
+    EventWrapper.find('.show-details').simulate('click');
+    expect(EventWrapper.find(EventDetails)).toHaveLength(1);
+  });
 
-//   test('can click show details to expand EventDetails', () => {
-//     EventWrapper.setState({ showDetails: false });
-//     EventWrapper.find('.show-details').simulate('click');
-//     expect(EventWrapper.find(EventDetails)).toHaveLength(1);
-//   });
-
-//   test('while details are expanded, clicking showDetails button will collapse details', () => {
-//     EventWrapper.setState({ showDetails: true });
-//     EventWrapper.find('.hide-details').simulate('click');
-//     expect(EventWrapper.find(EventDetails)).toHaveLength(0);
-//   });
-// });
+  test('while details are expanded, clicking showDetails button will collapse details', () => {
+    EventWrapper.setState({ showDetails: true });
+    EventWrapper.find('.hide-details').simulate('click');
+    expect(EventWrapper.find(EventDetails)).toHaveLength(0);
+  });
+});
