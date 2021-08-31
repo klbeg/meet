73c82003701e1a1ba7f80beccf18b56ad92c601d
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-08-31 14:40:38 -0400

    BDD toggleEventDetails •tests completed and passing @ 100%

--- src/features/toggleEventDetails.test.js
+++ src/features/toggleEventDetails.test.js
@@ -36,17 +36,45 @@ defineFeature(feature, (test) => {
     });
   });
   test('User can expant event details', ({ given, when, then }) => {
-    given('The event details are collapsed', () => {});
+    given('The event details are collapsed', () => {
+      AppWrapper = mount(<App />);
+      expect(
+        AppWrapper.find(EventList).find(Event).find(EventDetails)
+      ).toHaveLength(0);
+    });
 
-    when('The user clicks the show-details button', () => {});
+    when('The user clicks the show-details button', () => {
+      AppWrapper.setState({ numOfEvents: 1 });
+      EventWrapper = AppWrapper.find(EventList).find(Event);
+      EventWrapper.find('.show-details').simulate('click');
+    });
 
-    then('Event details should be expanded for that event', () => {});
+    then('Event details should be expanded for that event', () => {
+      expect(
+        AppWrapper.find(EventList).find(Event).find(EventDetails)
+      ).toHaveLength(1);
+    });
   });
   test('User can collapse the event details', ({ given, when, then }) => {
-    given('Event details are expanded', () => {});
+    given('Event details are expanded', () => {
+      AppWrapper.setState({ numOfEvents: 1 });
+      EventWrapper = AppWrapper.find(EventList).find(Event);
+      EventWrapper.setState({ showDetails: true });
+      expect(
+        AppWrapper.find(EventList).find(Event).find(EventDetails)
+      ).toHaveLength(1);
+    });
 
-    when('The user clicks the hide-details button', () => {});
+    when('The user clicks the hide-details button', () => {
+      AppWrapper.setState({ numOfEvents: 1 });
+      EventWrapper = AppWrapper.find(EventList).find(Event);
+      EventWrapper.find('.hide-details').simulate('click');
+    });
 
-    then('The event details should be collapsed', () => {});
+    then('The event details should be collapsed', () => {
+      expect(
+        AppWrapper.find(EventList).find(Event).find(EventDetails)
+      ).toHaveLength(0);
+    });
   });
 });
