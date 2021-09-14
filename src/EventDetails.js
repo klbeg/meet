b387d75647c9d7b734f1c34844223d85ca6c33ae
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-09-14 18:43:47 -0400

    Updates

--- src/EventDetails.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class EventDetails extends Component {
-  render() {
-    return <div>{this.props.event.description}</div>;
-  }
-}
-
-export default EventDetails;
