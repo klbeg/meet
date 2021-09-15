6e6ed8dd1ddbc88399af87ceddd5eea937487479
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-09-15 15:55:14 -0400

    implemented atatus: imported, downloaded, etc.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as atatus from 'atatus-spa';
 import './index.css';
 import App from './App';
+
+atatus.config('2328982937db4a178c5c9e1d6e2affbc').install();
 
 ReactDOM.render(
   <React.StrictMode>
