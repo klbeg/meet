d4a91278a03a0a7a7a81222a3d4582946ffc7b55
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-09-15 18:20:24 -0400

    created Alerts.js -adds alert class with a variable text color -added an alert to CitySearch for when the typed city isn't found

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,37 +1,49 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   state = {
     query: '',
     suggestions: [],
     showSuggestions: undefined,
+    infoText: '',
   };
 
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
+      suggestions: [],
       showSuggestions: false,
+      infoText: '',
     });
     this.props.updateEvents(suggestion);
   };
 
   handleInputChange = (event) => {
     const value = event.target.value;
-
-    //  Ask why indexOf.(value.toUpperCase()) > -1;
-    //  I don't know what this does, or why
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText:
+          'We can not find the city your are looking for.  Please try another city.',
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: '',
+      });
+    }
   };
 
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input
           className="city"
           placeholder="CitySearch"
