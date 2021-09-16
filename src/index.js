45431d2619d0edf1f273b59331a6868f33a5d0ed
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-09-16 15:51:27 -0400

    added service worker. -getting error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import * as atatus from 'atatus-spa';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './service-worker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 atatus.config('2328982937db4a178c5c9e1d6e2affbc').install();
 
@@ -12,3 +14,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+ServiceWorkerRegistration.register();
