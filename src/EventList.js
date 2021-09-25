e1ca2901f5a2e6449c7c4643358ecb609b6884ae
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-09-25 12:37:38 -0400

    Adding 'app offline' alert

--- src/EventList.js
+++ src/EventList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Event from './Event';
+import { InfoAlert } from './Alert';
 
 class EventList extends Component {
   state = {
@@ -9,21 +10,24 @@ class EventList extends Component {
 
   render() {
     return (
-      <ul className="event-list">
-        {this.props.events
-          .slice(
-            0,
-            this.props.numOfEvents
-              ? this.props.numOfEvents
-              : this.state.eventsLength
-          )
-          .map((event, i) => (
-            <li key={event.id}>
-              <span>{i}</span>
-              <Event event={event} />
-            </li>
-          ))}
-      </ul>
+      <div className="event-list-container">
+        <InfoAlert>Page is appearing offline</InfoAlert>
+        <ul className="event-list">
+          {this.props.events
+            .slice(
+              0,
+              this.props.numOfEvents
+                ? this.props.numOfEvents
+                : this.state.eventsLength
+            )
+            .map((event, i) => (
+              <li key={event.id}>
+                <span>{i}</span>
+                <Event event={event} />
+              </li>
+            ))}
+        </ul>
+      </div>
     );
   }
 }
