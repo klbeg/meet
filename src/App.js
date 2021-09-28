e4a6fc4ab87461e7131d19d3cdd8dc54d20039b0
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-09-28 15:56:15 -0400

    -added chart for data visualization -installed rechart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
 
 import './App.css';
 import EventList from './EventList';
@@ -42,6 +50,19 @@ class App extends Component {
     this.mounted = false;
   }
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split(', ').shift();
+
+      return { city, number };
+    });
+    return data;
+  };
+
   updateNumberOfEvents = (value) => {
     this.setState({
       numOfEvents: value,
@@ -70,6 +91,27 @@ class App extends Component {
           updateEvents={this.updateEvents}
         />
         <NumberOfEvents updateNumberOfEvents={this.updateNumberOfEvents} />
+        <ScatterChart
+          width={400}
+          height={400}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis
+            type="number"
+            dataKey="number"
+            name="number of events"
+            allowDecimals={false}
+          />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
         <EventList
           events={this.state.events}
           numOfEvents={this.state.numOfEvents}
